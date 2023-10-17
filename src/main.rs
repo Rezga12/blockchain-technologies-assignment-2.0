74c5dfd5af605f51dfa9d13d79f3d844eff1e4a6
Rezga12 <rmeis16@freeuni.edu.ge>
2023-10-17 11:33:58 +0400

    finished design document

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+const PRIVATE_KEY: &str = "";
+
+#[tokio::main]
+async fn main() {
+    send("<P2PKH testnet address>", 1000).await;
+}
+
+async fn send(receiver_address: &str, amount_satoshis: u64) {
+    todo!("Just make this function work")
 }
